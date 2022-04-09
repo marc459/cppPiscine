8e8ab0b4ca1907ca9d3415d41d93f4d84eb1e7c8
Marcos Santos serrano <santosserranomarcos@gmail.com>
2022-04-09 22:38:05 +0200

    ex00

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,12 +6,13 @@
 /*   By: msantos- <msantos-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 19:09:52 by msantos-          #+#    #+#             */
-/*   Updated: 2022/04/07 20:49:39 by msantos-         ###   ########.fr       */
+/*   Updated: 2022/04/09 22:37:27 by msantos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cctype>
+#include <stdio.h>
 
 int    main(int argc, char **argv)
 {
@@ -21,10 +22,15 @@ int    main(int argc, char **argv)
     x = 1;
     y = 0;
     if(argc == 1)
-        cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     while(argv[x])
     {
-        std::cout << std::toupper(argv[x]);
+        while(argv[x][y])
+        {
+            std::cout << (char)std::toupper(argv[x][y]);
+            y++;
+        }
+        y = 0;
         x++;
 
     }
