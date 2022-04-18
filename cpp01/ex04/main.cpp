1f24ca42b91d4f2ef0104383ff0a4741ca21bcf7
Marcos Santos Serrano <santosserranomarcos@gmail.com>
2022-04-18 11:50:33 +0200

    ex04 cpp01 unless file a a

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: marcos <marcos@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 20:22:20 by marcos            #+#    #+#             */
-/*   Updated: 2022/02/23 16:54:34 by marcos           ###   ########.fr       */
+/*   Updated: 2022/04/18 11:49:29 by marcos           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ std::string   replace_line(std::string &line, std::string s1, std::string s2)
 
 int    main(int argc, char **argv)
 {
-    if(argc != 4)
+    if(argc != 4 || argv[2][0] == '\0' || argv[3][0] == '\0')
     {
         std::cout << "numero de argumentos incorrectos" << std::endl;
         return (1);
@@ -38,7 +38,7 @@ int    main(int argc, char **argv)
     std::fstream myfile;
     std::fstream newfile;
     std::string filename = (std::string)argv[1];
-    std::string newfilename = "replace." + (std::string)argv[1];
+    std::string newfilename =  (std::string)argv[1] + ".replace";
     std::string line;
     std::string replacedline;
     std::string s1 = (std::string)argv[2];
@@ -46,10 +46,9 @@ int    main(int argc, char **argv)
 
     myfile.open(filename, std::ios::in);
     newfile.open(newfilename, std::ios::out);
-    while (std::getline (myfile, line)) {
+    while (std::getline(myfile, line)) {
         if(line.find(s1) != -1)
         {
-            std::cout << line.find(s1) <<  "finded replacedline on " << line << std::endl;
             replacedline = replace_line(line,s1,s2);
             newfile << replacedline << "\n";
         }
