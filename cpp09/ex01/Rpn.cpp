40b2bb98a3837936104d2c90e89d019d75fb6861
Marcos Santos serrano <santosserranomarcos@gmail.com>
2023-05-01 21:10:50 +0200

    ex01 probably done testing it

--- cpp09/ex01/Rpn.cpp
+++ cpp09/ex01/Rpn.cpp
@@ -2,24 +2,34 @@
 
 Rpn::Rpn(std::string expresion)
 {
+    int op1;
+    int op2;
     for (size_t i = 0; i < expresion.size(); i++) {
         char c = expresion[i];
         if (isdigit(c)) {
             int num = c - '0';
             this->polishStack.push(num);
         } else if (c == '+' || c == '-' || c == '*' || c == '/') {
-            int op1 = this->polishStack.top();
-            this->polishStack.pop();
-            int op2 = this->polishStack.top();
-            this->polishStack.pop();
-            int res;
-            switch (c) {
-                case '+': res = op2 + op1; break;
-                case '-': res = op2 - op1; break;
-                case '*': res = op2 * op1; break;
-                case '/': res = op2 / op1; break;
+            
+            if(!this->polishStack.empty())
+            {
+                op1 = this->polishStack.top();
+                this->polishStack.pop();
             }
-            this->polishStack.push(res);
+            if(!this->polishStack.empty())
+            {
+                op2 = this->polishStack.top();
+                this->polishStack.pop();
+
+                int res;
+                switch (c) {
+                    case '+': res = op2 + op1; break;
+                    case '-': res = op2 - op1; break;
+                    case '*': res = op2 * op1; break;
+                    case '/': res = op2 / op1; break;
+                }
+                this->polishStack.push(res);
+            }
         }
         else if (c != ' ')
         {
@@ -27,7 +37,10 @@ Rpn::Rpn(std::string expresion)
             return;
         }
     }
-    std::cout <<  this->polishStack.top() << std::endl;
+    if(!this->polishStack.empty())
+        std::cout <<  this->polishStack.top() << std::endl;
+    else
+        std::cout <<  "Error" << std::endl;
 }
 
 Rpn::~Rpn()
