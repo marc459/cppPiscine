7098b1a4f2347e3138f4bb8615f242604bdeca64
Marcos Santos serrano <santosserranomarcos@gmail.com>
2023-05-26 00:53:07 +0200

    Error check ex02

--- cpp09/ex02/README.md
+++ cpp09/ex02/README.md
@@ -1,4 +1,14 @@
-# BIBILOGRAPHY
+### PMERGEME
+
+Your program must use a merge-insert sort algorithm to sort the positive integer
+sequence.
+
+In this case we use vctor and deque containers to fill in diferent positions.
+
+deque does not guarantee the same constant-time random access, as it divides its elements into multiple blocks of memory. 
+
+Considering the characteristics mentioned above, vector is often a preferable choice for implementing the merge sort algorithm due to its superior random access performance. Merge sort involves frequent splitting and merging of subarrays, which benefit from efficient random access to perform these operations effectively. However, it's important to note that the actual performance difference between vector and deque may vary depending on the size of the input and the specific implementation details.
+### BIBILOGRAPHY
 
 https://www.javatpoint.com/merge-sort#:~:text=Merge%20sort%20is%20similar%20to,merges%20the%20two%20sorted%20halves.
 
--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -6,28 +6,36 @@
 /*   By: msantos- <msantos-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 21:15:01 by msantos-          #+#    #+#             */
-/*   Updated: 2023/05/23 22:02:59 by msantos-         ###   ########.fr       */
+/*   Updated: 2023/05/26 00:52:09 by msantos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "PmergeMe.hpp"
 
+bool isNumeric(const std::string& str) {
+    for (std::string::const_iterator it = str.begin(); it != str.end(); ++it) {
+        if (*it < '0' || *it > '9') {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main(int argc, char **argv)
 {
     
-    if(argc > 0)
+    if(argc > 1)
     {
-        int arr[argc -1];
-        for(int i = 1; i < (argc -  1); i++)
+        int arr[argc];
+        for(int i = 1; i < argc; i++)
         {
             try {
-				arr[i] = std::stoi(argv[i]);
-				if (arr[i] < 0)
-				{
-					std::cerr << "Error" << std::endl;
-					return 1;
-				}	
+				if(!isNumeric(argv[i]))
+					throw std::runtime_error("Error: String is not numeric");
+				arr[i -1] = std::stoi(argv[i]);
+				if (arr[i - 1] < 0)
+					throw std::runtime_error("Error");
 			}
 			catch(std::invalid_argument &e){
 				std::cout << e.what() << std::endl;
@@ -36,6 +44,10 @@ int main(int argc, char **argv)
 			catch(std::out_of_range &e){
 				std::cout << e.what() << std::endl;
 				return 1;	
+			}
+			catch (const std::exception& e) {
+				std::cout << e.what() << std::endl;
+				return 1;
 			}
         }
         PmergeMe *sort1 = new PmergeMe(arr, argc-1);
